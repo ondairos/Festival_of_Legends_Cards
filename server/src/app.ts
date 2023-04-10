24843cf252b3061a9b824c9a74963389b27cd146
Ondairos <ondairos@gmail.com>
2023-04-10 16:14:49 +0300

    comments and gitignore

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,19 @@
 // imports for typescript express.
 import express, { NextFunction, Application, Request, Response } from 'express'
 
-
 const app: Application = express()
+// code
+
+const add = (a: number, b: number): number => a + b
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    console.log(add(5, 5));
     res.send('Hello')
 })
 
+
+
+// listen for server
 app.listen(5000, () => {
     console.log(`Server running at port: 5000`);
 
