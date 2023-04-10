a1a872a9d53091ea2dc5ac1feffdcec6e31a006a
Ondairos <ondairos@gmail.com>
2023-04-10 17:49:05 +0300

    fetched data from server and display

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const [cardData, setCardData] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchAllData = async () => {
+      const allDataFetch = await axios.get("/api/druid");
+      setCardData(allDataFetch.data);
+      console.log(cardData);
+    };
+    fetchAllData();
+  }, []);
 
   return (
     <div className="App">
-      <span>asdsa</span>
+      <h2>Druid</h2>
+      {cardData.map((element) => {
+        return (
+          <div key={element.cardId}>
+            <li>{element.cardName}</li>
+            <img src={element.cardImg} alt="image" />
+          </div>
+        );
+      })}
     </div>
   );
 }
