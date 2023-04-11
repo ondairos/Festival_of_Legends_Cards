bf3ba005861fbf99d274b56e815694e60bc4823d
Ioannis Kantiloros <Ondairos@gmail.com>
2023-04-12 01:41:06 +0300

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       '/api': {
         target: 'https://fol-api.onrender.com',
         changeOrigin: true,
-        secure: true,
+        secure: false,
         ws: true
       }
     }
